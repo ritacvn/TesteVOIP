7505398a16b492f46fcfa90a0eab524f411ae8b7
Rita <ritadecassia.vasconcelos@hotmail.com>
2020-01-08 22:16:54 -0400

    Created files to make the request for the JSON api
    Signed-off-by: Rita <ritadecassia.vasconcelos@hotmail.com>

---
+++ TesteVOIP/Model/Photo.swift
@@ -0,0 +1,9 @@
+//
+//  Photo.swift
+//  TesteVOIP
+//
+//  Created by Rita de Cássia Vasconcelos do Nascimento on 08/01/20.
+//  Copyright © 2020 Rita de Cássia Vasconcelos do Nascimento. All rights reserved.
+//
+
+import Foundation

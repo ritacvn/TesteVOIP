8155dc9a36740a92211472f1eda0ec182bb83121
Rita <ritadecassia.vasconcelos@hotmail.com>
2020-01-09 17:59:16 -0400

    WIP: Making an image show up on the next view controller but it's still not the correct image
    Signed-off-by: Rita <ritadecassia.vasconcelos@hotmail.com>

--- TesteVOIP/Controller/PhotoDetailVC.swift
+++ TesteVOIP/Controller/PhotoDetailVC.swift
@@ -10,10 +10,14 @@ import UIKit
 
 class PhotoDetailVC: UIViewController {
 
+    var imageView : UIImageView?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .brown
+        view.backgroundColor = .purple
+        imageView  = UIImageView(frame: CGRect(x: 5, y: 5, width: 200, height: 200))
+        imageView?.image = UIImage(named:"268x0w")
+        self.view.addSubview(imageView!)
     }
     
 
